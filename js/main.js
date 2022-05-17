926452009dafc80c7e1ee1c60512fc33e6281f77
mirtalonghi <51256133+mirtalonghi@users.noreply.github.com>
2022-05-16 23:37:03 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+function toggleMobileMenu() {
+    const menu = document.getElementById('mobile-menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
